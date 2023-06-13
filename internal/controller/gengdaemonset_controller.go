633497c17bcb09675a392df7d7c27e2f8774145e
gengqianyu <gengqianyu@gmail.com>
2023-06-13 12:34:25 +0800

    test rebase main

--- internal/controller/gengdaemonset_controller.go
+++ internal/controller/gengdaemonset_controller.go
@@ -93,7 +93,7 @@ func (r *GengDaemonsetReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 			panic(err)
 		}
 	}
-	//测试 git rebase on origin myfeature 1
+	//测试 git rebase on origin myfeature
 	return ctrl.Result{}, nil
 }
 
